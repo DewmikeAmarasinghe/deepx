35eeb099031468a2a55c33999e08d6a51b45cf03
dewmike <dewmikela@gmail.com>
2026-05-05 10:37:29 +0530

    update

--- src/deepx_cli/_utils/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Shared helpers for terminal chat (non-widget)."""
-
-from deepx_cli._utils.layout import section_break
-from deepx_cli._utils.stream_items import agent_label, tool_name_and_arguments
-
-__all__ = ["agent_label", "section_break", "tool_name_and_arguments"]
